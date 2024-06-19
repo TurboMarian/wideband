00bce84f10beae7ae61a1ed9aa7cb9ff25d5ab8e
TurboMarian <podgajnymariusz@gmail.com>
2024-06-19 22:35:40 +0100

    Update io_pins.h

--- firmware/boards/f1_dual/io/io_pins.h
+++ firmware/boards/f1_dual/io/io_pins.h
@@ -1,42 +1,42 @@
 #pragma once
 
-#define LED_BLUE_PORT GPIOB
-#define LED_BLUE_PIN 13
-#define LL_LED_BLUE_PIN LL_GPIO_PIN_13
+#define LED_BLUE_PORT GPIOA
+#define LED_BLUE_PIN 8
+#define LL_LED_BLUE_PIN LL_GPIO_PIN_8
 
-#define LED_GREEN_PORT GPIOA
-#define LED_GREEN_PIN 8
+#define LED_GREEN_PORT GPIOC
+#define LED_GREEN_PIN 9
 
 // Communication - UART
-#define UART_GPIO_PORT				GPIOA
-#define LL_UART_TX_PIN				LL_GPIO_PIN_9
-#define LL_UART_RX_PIN				LL_GPIO_PIN_10
+#define UART_GPIO_PORT				GPIOC
+#define LL_UART_TX_PIN				LL_GPIO_PIN_10
+#define LL_UART_RX_PIN				LL_GPIO_PIN_11
 
 // Communication - CAN1
-#define CAN_GPIO_PORT				GPIOA
-#define LL_CAN_TX_PIN				LL_GPIO_PIN_12
-#define LL_CAN_RX_PIN				LL_GPIO_PIN_11
+#define CAN_GPIO_PORT				GPIOB
+#define LL_CAN_TX_PIN				LL_GPIO_PIN_9
+#define LL_CAN_RX_PIN				LL_GPIO_PIN_8
 
 // LSU 4.2 - 6.8K
 #define NERNST_42_ESR_DRIVER_PORT   GPIOB
-#define NERNST_42_ESR_DRIVER_PIN    12
+#define NERNST_42_ESR_DRIVER_PIN    14
 
 // LSU 4.9 - 22K
 #define NERNST_49_ESR_DRIVER_PORT	GPIOB
-#define NERNST_49_ESR_DRIVER_PIN	11
+#define NERNST_49_ESR_DRIVER_PIN	13
 
-#define NERNST_49_BIAS_PORT			GPIOB
+#define NERNST_49_BIAS_PORT			GPIOC
 #define NERNST_49_BIAS_PIN			2
 
 // LSU ADV - 47K
 #define NERNST_ADV_ESR_DRIVER_PORT	GPIOB
-#define NERNST_ADV_ESR_DRIVER_PIN	10
+#define NERNST_ADV_ESR_DRIVER_PIN	12
 
 // L_heater_pwm - PB7 TIM4_CH2
-#define HEATER_PWM_DEVICE			PWMD4
-#define HEATER_PWM_CHANNEL_0		1
+#define HEATER_PWM_DEVICE			PWMD1
+#define HEATER_PWM_CHANNEL_0		2
 #define L_HEATER_PORT				GPIOB
-#define L_HEATER_PIN				7
+#define L_HEATER_PIN				15
 
 // R_heater_pwm - PB6 TIM4_CH1
 #define HEATER_PWM_CHANNEL_1		0
@@ -57,12 +57,12 @@
 // CH2N and CH3N are complementary outputs
 #define AUXOUT_DAC_PWM_OUTPUT_MODE	PWM_COMPLEMENTARY_OUTPUT_ACTIVE_LOW
 
-#define ID_SEL1_PORT				GPIOC
-#define ID_SEL1_PIN					13
+#define ID_SEL1_PORT				GPIOB
+#define ID_SEL1_PIN					2
 
 // PC15 - SPI1_CS1 - L_EGT
-#define EGT_CS0_PORT				GPIOC
-#define EGT_CS0_PIN					15
+#define EGT_CS0_PORT				GPIOB
+#define EGT_CS0_PIN					6
 // PC14 - SPI1_CS0 - R_EGT
-#define EGT_CS1_PORT				GPIOC
-#define EGT_CS1_PIN					14
+#define EGT_CS1_PORT				GPIOB
+#define EGT_CS1_PIN					7
